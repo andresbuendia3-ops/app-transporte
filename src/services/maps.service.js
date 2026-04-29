714fdc33c0b95218b14b0132656a4cbf1c464e27
Andres Buendia <andresbuendia3@hotmail.com>
2026-04-29 15:10:12 -0500

    backend api key fixed

--- src/services/maps.service.js
+++ src/services/maps.service.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getDistance = async (origin, destination, tipoVehiculo) => {
 
-  const apiKey = "AIzaSyAGjC1Eo3ldxv1wWBMDMdzkfqUZPL3TDM0";
+ const apiKey = "AIzaSyDNUs1DfDZyHUVbyEs0OOXmERKWDTO-DXA";
 
   const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
 
