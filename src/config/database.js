1bf0137e0c0f6bc0095dc555fa9df99d7809e835
Andres Buendia <andresbuendia3@hotmail.com>
2026-04-29 23:51:51 -0500

    database rebuild final

--- src/config/database.js
+++ src/config/database.js
@@ -21,6 +21,16 @@ db.serialize(() => {
     )
   `);
 
+  db.run(`
+    CREATE TABLE IF NOT EXISTS usuarios (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nombre TEXT,
+      correo TEXT UNIQUE,
+      password TEXT,
+      rol TEXT
+    )
+  `);
+
 });
 
 module.exports = db;
